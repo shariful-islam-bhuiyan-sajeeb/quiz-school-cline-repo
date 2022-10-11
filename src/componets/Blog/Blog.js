c19948314853da22f3f8e4f1f535096d55f0068b
shariful4345 <mdsajeeb4345@gmail.com>
2022-10-11 23:52:18 +0600

    added toast

--- src/componets/Blog/Blog.js
+++ src/componets/Blog/Blog.js
@@ -3,7 +3,30 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h3>This Is Blog page.</h3>
+            <div tabIndex={0} className="collapse w-3/4 mx-auto mt-8 collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-2xl font-semibold">
+                    What is the purpose of react router?
+                </div>
+                <div className="collapse-content text-xl">
+                    <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse  collapse-arrow w-3/4 mx-auto border  border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-2xl font-semibold">
+                    How does context api work?
+                </div>
+                <div className="collapse-content text-xl">
+                    <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse  collapse-arrow w-3/4 mx-auto border  border-base-300     bg-base-100 rounded-box">
+                <div className="collapse-title text-2xl font-semibold">
+                    How does context api work?
+                </div>
+                <div className="collapse-content text-xl">
+                    <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/componets/Questions/Questions.js
+++ src/componets/Questions/Questions.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const Questions = ({ quizQuestion }) => {
     console.log(quizQuestion);
-    const { question, options } = quizQuestion;
+
+    const { question, options, correctAnswer } = quizQuestion;
+    // console.log(question);
+
+    const questionError = correctAnswer.replaceAll(' ','')
+    const getInnerText =(btn)=>{
+        const btnError = btn.replaceAll(' ','');
+        
+        
+        if(questionError === btnError){
+           toast.success('Congratulation your!!',{autoClose:500} )
+        }else{
+            toast.warning('Wrong answer your!!', { autoClose: 500 })
+        }
+
+    }
   
     return (
-        <div className='border-2 border-gray-300 rounded-lg px-4 py-6 my-20 lg:w-3/4 mx-auto md:w-11/12  text-center text-2xl '> 
+        <div className='border-2 border-gray-300 rounded-lg px-4 py-6 my-20 lg:w-3/4 mx-auto md:w-11/12  text-center text-2xl  shadow-xl '> 
             <div>
                 <p className='font-semibold pb-6 '> {question}</p>
-
+                
             </div>
              <div className='grid md:grid-cols-2 gap-4 pt-4'>
                 {
-                    options.map((option, idx) => <div className='bg-gray-200 rounded-lg' key={idx} >
-                        <button className='py-3 w-full h-full'>{option}</button>
+                    options.map((option,idx) => <div className='bg-gray-200 rounded-lg shadow- shadow-lg' key={idx} >
+                        <button onClick={(e) => getInnerText(e.target.innerText) } className='py-3 w-full h-full'> <input className='' type="radio" /> {option}</button>
                      </div>)  
                 }
                 
